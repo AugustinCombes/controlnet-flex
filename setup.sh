72dd3ef64d9830dd462436209238fdf484702d63
AugustinCombes <65305236+AugustinCombes@users.noreply.github.com>
2025-01-22 13:34:20 +0100

    fix setup with python3.11 install

--- setup.sh
+++ setup.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
 
+apt update
+apt install python3.11 python3.11-venv
+
 curl -sSL https://install.python-poetry.org | POETRY_HOME=/etc/poetry python3 -
+
+echo 'export PATH="/etc/poetry/bin:$PATH"' >> ~/.bashrc
 export PATH="/etc/poetry/bin:$PATH"
+source ~/.bashrc
+
 poetry install
